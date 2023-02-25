105892a7a23519004bf6c7a52ab0b1dc253bf010
Nikita <nikita.umnov@mas-tech.ru>
2023-02-25 15:07:03 +0400

    feat: start developing method which makes both range and ambience measures. feat: начал разрабатывать метод который совершает измеренения расстояния и освещенности.

--- js/module/ModuleVL6180X.js
+++ js/module/ModuleVL6180X.js
@@ -1,4 +1,5 @@
 const ClassVL6180XDefault = require('ModuleVL6180XDefault')._class;
+const ClassAppError = require('https://raw.githubusercontent.com/Konkery/ModuleAppError/main/js/module/ModuleAppError.js');
 
 class ClassVL6180X extends ClassVL6180XDefault {
   //_opts: {i2c: I2C1, irqPin: irqPin}
@@ -11,6 +12,18 @@ class ClassVL6180X extends ClassVL6180XDefault {
 
 ClassVL6180X.prototype.testMethod = function(){
   return true;
-}
+};
+
+ClassVL6180X.prototype.startDualMeasures = function(){
+    setInterval(() => {
+        this.range((err, value) => {
+            if (err) {
+                console.log(Infinity);
+            } else {
+                console.log(value);
+            }
+        });
+    }, 40);
+};
 
 exports = ClassVL6180X;
