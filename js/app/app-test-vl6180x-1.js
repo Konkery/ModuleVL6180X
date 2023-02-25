69860127c4e7f34f2db5bc07c19d9c0446cc1035
Nikita <nikita.umnov@mas-tech.ru>
2023-02-25 19:54:54 +0400

    fix: delete dummy "testMethod"

--- js/app/app-test-vl6180x-1.js
+++ js/app/app-test-vl6180x-1.js
@@ -3,4 +3,4 @@ I2C1.setup({sda: SDA, scl: SCL, bitrate: 400000}); //нaстройка i2c
 const ClassVL6180X = require('ModuleVL6180X'); //экспорт класса из моего модуля
 const vl6180x = new ClassVL6180X({i2c: I2C1, irqPin: P4});
 
-console.log(vl6180x.testMethod()); //вызов тестового метода 
+vl6180x.startDualMeasures();
