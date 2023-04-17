155e361117df0c95f686dee9419900b23cc7afd0
Nikita <nikita.umnov@mas-tech.ru>
2023-04-17 20:37:25 +0400

    feat: updated example app

--- js/app/app-test-new_vl6180x.js
+++ js/app/app-test-new_vl6180x.js
@@ -1,8 +1,7 @@
 const err = require('https://raw.githubusercontent.com/Konkery/ModuleAppError/main/js/module/ModuleAppError.js');
 require('https://raw.githubusercontent.com/Konkery/ModuleAppMath/main/js/module/ModuleAppMath.min.js').is();
 
-const ClassBaseVL6180 = require('ModuleVL6180.min');
-const ClassVL6180 = require('ModuleVL6180.min').ClassVL6180;
+const ClassVL6180 = require('https://raw.githubusercontent.com/Nicktonious/ModuleVL6180X/fork-Nikita/js/module/ModuleVL6180.js').ClassVL6180;
 const ClassBaseI2CBus = require('ClassBaseI2CBus.min');
 const I2Cbus = new ClassBaseI2CBus();
 let bus = I2Cbus.AddBus({sda:B9 , scl:B8, bitrate:100000 });
@@ -12,4 +11,4 @@ let vl = new ClassVL6180({i2c: bus.IDbus, irqPin: P4});
 vl.StartRange(50);
 setTimeout(() => {vl.StartALS(200);}, 10000);
 setTimeout(() => {vl.StartDual(200);}, 20000);
-console.log(vl.Range, vl.Illuminance);
+// console.log(vl.Range, vl.Illuminance);
